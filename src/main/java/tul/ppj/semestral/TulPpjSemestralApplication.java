1a17952f26323c01a8991f018a0e33c30327564b
arbyys <adam.petricek@centrum.cz>
2025-04-06 21:56:14 +0200

    Initial commit

---
+++ src/main/java/tul/ppj/semestral/TulPpjSemestralApplication.java
@@ -0,0 +1,13 @@
+package tul.ppj.semestral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TulPpjSemestralApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TulPpjSemestralApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tul/ppj/semestral/TulPpjSemestralApplicationTests.java
@@ -0,0 +1,13 @@
+package tul.ppj.semestral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TulPpjSemestralApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
